d8c1f0f4d0c661c4cbf209bdc64023a3264c45a1
tnj96 <talesha267@gmail.com>
2019-02-15 13:31:23 +0000

    Business

--- Classroom/src/main/java/com/qa/business/service/ClassroomServiceImpl.java
+++ Classroom/src/main/java/com/qa/business/service/ClassroomServiceImpl.java
@@ -1,5 +1,41 @@
 package com.qa.business.service;
 
-public class ClassroomServiceImpl {
+public class ClassroomServiceImpl implements ClassroomService{
+
+	@Override
+	public String createClassroom(String trainerName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAllClassrooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAClassroom(int classroomId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateClassroom(String trainerName, int classroomId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteClassroom(int classroomId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int cycleClassrooms(String trainerName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- Classroom/src/main/java/com/qa/business/service/TraineeServiceImpl.java
+++ Classroom/src/main/java/com/qa/business/service/TraineeServiceImpl.java
@@ -1,5 +1,41 @@
 package com.qa.business.service;
 
-public class TraineeServiceImpl {
+public class TraineeServiceImpl implements TraineeService{
+
+	@Override
+	public String createTrainee(String traineeName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAllTrainees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getATrainer(int traineeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateTrainee(String traineeName, int traineeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteTrainee(int traineeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int cycleTrainees(String traineeName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
